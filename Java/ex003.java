b7dedfc40e0d7529d8cce287ca0048694e00fbde
Lucas <lucas.ed.pe.silva@gmail.com>
2024-03-09 14:51:17 -0300

    tentativa do javafx

---
+++ Java/ex003.java
@@ -0,0 +1,15 @@
+package Java;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ex003 extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage arg0) throws Exception {
+        
+    }
+}
